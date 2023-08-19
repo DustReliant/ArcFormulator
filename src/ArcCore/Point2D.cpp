f06826591c648ea10da4211dda918462c01ba8bd
‘1619513467’ <1619513467@qq.com>
2023-08-20 02:55:00 +0800

    初步实现Ponint2D类

--- src/ArcCore/Point2D.cpp
+++ src/ArcCore/Point2D.cpp
@@ -9,3 +9,35 @@ Point2D::~Point2D()
 {
 
 }
+
+void Point2D::setX(double x) 
+{
+    m_x = x;
+}
+
+void Point2D::setY(double y) 
+{
+    m_y = y;
+}
+
+double Point2D::getX() const 
+{
+    return m_x;
+}
+
+double Point2D::getY() const 
+{
+    return m_y;
+}
+
+double Point2D::distanceTo(const Point2D &other) const 
+{
+    double dx = m_x - other.m_x;
+    double dy = m_y - other.m_x;
+    return std::sqrt(dx * dx + dy * dy);
+}
+
+void Point2D::print() const 
+{
+    std::cout << "(" << m_x << ", " << m_y << ")" << std::endl;
+}
--- src/ArcCore/Point2D.h
+++ src/ArcCore/Point2D.h
@@ -1,9 +1,42 @@
-#pragma once
+#ifndef POINT2D_H
+#define POINT2D_H
 
+#include <cmath>
+#include <iostream>
 
-class Point2D
+class Point2D 
 {
 public:
-	Point2D();
-	~Point2D();
+    // 默认构造函数，将点初始化为原点 (0, 0)
+    Point2D() : m_x(0), m_y(0){}
+
+    // 参数构造
+    Point2D(double x, double y);
+
+    // 拷贝构造
+    Point2D(const Point2D &point2d);
+
+    // 设置点的x坐标
+    void setX(double x);
+
+    // 设置点的y坐标
+    void setY(double y);
+
+    // 获取点的x坐标
+    double getX() const;
+
+    // 获取点的y坐标
+    double getY() const;
+
+    // 计算与另一点的距离
+    double distanceTo(const Point2D &other) const;
+
+    // 输出点的坐标
+    void print() const;
+
+private:
+    double m_x;
+    double m_y;
 };
+
+#endif // POINT2D_H
