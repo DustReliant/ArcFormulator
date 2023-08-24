7445cc909103acf88aad76d82d0cb2cb865dcc56
‘1619513467’ <1619513467@qq.com>
2023-08-24 22:56:26 +0800

    菜单管理器UI文件

---
+++ src/ui/ribbon/register/QArcMenuManagerDialog.cpp
@@ -0,0 +1,11 @@
+#include "QArcMenuManagerDialog.h"
+
+QArcMenuManagerDialog::QArcMenuManagerDialog(QWidget *parent)
+    : QDialog(parent), ui(new Ui::QArcMenuManagerDialog)
+{
+	ui->setupUi(this);
+}
+
+QArcMenuManagerDialog::~QArcMenuManagerDialog()
+{
+}
---
+++ src/ui/ribbon/register/QArcMenuManagerDialog.h
@@ -0,0 +1,41 @@
+
+/*
+* 
+* ≤Àµ•π‹¿Ì∆˜UI
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+*/
+
+
+
+
+#ifndef QARCMENUMANAGERDIALOG_H
+#define QARCMENUMANAGERDIALOG_H
+
+#include <QDialog>
+#include "ui_QArcMenuManagerDialog.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+    class QArcMenuManagerDialog;
+}
+QT_END_NAMESPACE
+
+class QArcMenuManagerDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	QArcMenuManagerDialog(QWidget *parent = nullptr);
+	~QArcMenuManagerDialog();
+
+private:
+	Ui::QArcMenuManagerDialog* ui;
+};
+#endif//QARCMENUMANAGERDIALOG_H
