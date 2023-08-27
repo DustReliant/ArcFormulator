4f4715c05fc6b1b1fc808254275dc996d31364a9
‘1619513467’ <1619513467@qq.com>
2023-08-27 18:59:57 +0800

    新建GemGeometry文件

---
+++ src/ArcCore/kernel/GemGeomotry.h
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+#ifndef GEMGEOMETRY_H
+#define GEMGEOMETRY_H
+namespace Kernel 
+{
+	class GemGeometry 
+	{
+	public:
+		virtual ~GemGeometry();
+
+
+
+
+
+
+
+	};
+} // namespace Kernel
+#endif // GEMGEOMETRY_H
